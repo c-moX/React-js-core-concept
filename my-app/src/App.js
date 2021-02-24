153f5e14bc155459c05808222ee34566d2df09f6
c-moX <cmoxsh@gmail.com>
2021-02-24 14:35:15 +0600

    React js core concepts file complete

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,29 +1,46 @@
+import React, { useEffect, useState } from 'react'; // Import for the UseEffect
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  // My Array In Objects
+  // My Array In Objects (01)
   const products = [
     {name: 'PhotoShop', price: '$89.99'},
-    {name: 'Visual Studio', price: '$80.99'},
-    {name: 'Illustrator', price: '$95.99'}
+    {name: 'Adobe PDF', price: '$45.99'},
+    // {name: 'Illustrator', price: '$95.99'}
   ];
+  // My Array Code (02)
+  const nayoks = ['Alomgir', 'Razzak', 'Rubel', 'Shakib'];
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello! <code>ReactApp,</code> You are welcome!</p>
 
-        {/* My Dynamic Code Is Here (01) */}
-        <Product product={products[0]}></Product>
-        <Product product={products[1]}></Product>
-        <Product product={products[2]}></Product>
+        {/* Display Dynamic Code Is Here (01) */}
+        {
+          products.map(pd => <Product product={pd}></Product>)
+        }
+
+        {/* Display Dynamic Code Is Here (02) */}
+        <ul>
+          {
+            nayoks.map(nayok => <li>{nayok}</li>)
+          }
+        </ul>
+
+        {/* Display Dynamic Code Is Here (03) */}
+        <Counter></Counter>
+
+        {/* Display Dynamic Code Is Here (04) */}
+        <Users></Users>
       </header>
     </div>
   );
 }
 
-// My Function Is Here (01)
+// Dynamic-Text && props-parameter Function Is Here (01)
 function Product(props) {
   const productStyles = {
     border: '1px solid gray',
@@ -40,6 +57,46 @@ function Product(props) {
       <h1>{name}</h1>
       <h3>{price}</h3>
       <button>Buy Now</button>
+    </div>
+  );
+}
+
+// EventListenner && useState count Function Is Here (03)
+function Counter(){
+  const [count, setCount] = useState(10); // Counter values && Set values
+  const handleIncrease = () => setCount(count + 1); // Increase button
+  const handleDecrease = () => setCount(count - 1); // Decrease button
+  // {
+  //   const newCount = count + 1;
+  //   setCount(newCount);
+  // };
+
+  return(
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={handleIncrease}>Increase</button>
+      <button onClick={handleDecrease}>Decrease</button>
+    </div>
+  );
+}
+
+// API && useState && count Function Is Here (04)
+function Users(){
+  const [users, setUsers] = useState([]);
+  useEffect(() =>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data));
+  }, [])
+
+  return(
+    <div>
+      <h3>Dynamic Users: {users.length}</h3>
+      <ol>
+        {
+          users.map(user => <li>{user.name}</li>)
+        }
+      </ol>
     </div>
   );
 }
